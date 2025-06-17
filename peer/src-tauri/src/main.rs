96149afb4b373571cfd8fd5ed3592644404baa99
Dhanvith Nayak <dhanvithnayak@proton.me>
2025-06-17 23:48:28 +0530

    implement log to file

--- peer/src-tauri/src/main.rs
+++ peer/src-tauri/src/main.rs
@@ -1,8 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tracing_appender::non_blocking::WorkerGuard;
+
+fn setup_logging() -> Option<WorkerGuard> {
+    if cfg!(debug_assertions) {
+        tracing_subscriber::fmt().with_writer(std::io::stdout).init();
+        None
+    } else {
+        let (writer, _guard) = {
+            let file_appender = tracing_appender::rolling::daily("logs", "hermes.log");
+            tracing_appender::non_blocking(file_appender)
+        };
+
+        tracing_subscriber::fmt().with_writer(writer).json().init();
+        Some(_guard)
+    }
+}
+
 
 fn main() {
-    tracing_subscriber::fmt().init();
+    let _guard = setup_logging();
     hermes_lib::run()
 }
